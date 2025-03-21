a25fde5c22c76dd53e7dcf40cf4179cf31c33aa8
markodjj <markodjordjevic244@gmail.com>
2025-03-21 19:47:47 +0100

    Test connectDB

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`MongoDb connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log("MongoDB connection error: ", error);
-    }
+  try {
+    console.log("0000011210");
+    console.log(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDb connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection error: ", error);
+  }
 };
